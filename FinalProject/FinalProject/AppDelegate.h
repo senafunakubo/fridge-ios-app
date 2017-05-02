382c5ff63118b88799e365fc9be245e880bd5ca7
Sena <inabacker24@gmail.com>
2017-05-01 18:18:05 -0700

    Fixed the layout of the top page

--- FinalProject/FinalProject/AppDelegate.h
+++ FinalProject/FinalProject/AppDelegate.h
@@ -12,7 +12,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (nonatomic, readonly, strong) FIRDatabaseReference * ref;
+@property (strong, nonatomic) FIRDatabaseReference *ref;
 
 
 @end
